22da2165b1d77dc3df040d371634f34ab9af0992
Sara Harvey <saraharveynyc@gmail.com>
2019-02-28 22:55:29 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,19 +1,5 @@
 module Findable
 
-  CONSTANT = []
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-    CONSTANT << self
-  end
-  end
- 
-  def all
-    CONSTANT
-  end
- 
   def find_by_name(name)
     CONSTANT.detect {|a| a.name == name}
   end
