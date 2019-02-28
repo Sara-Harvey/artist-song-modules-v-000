8fb3728c5f3dacd9f08f5b558477339f9090d252
Sara Harvey <saraharveynyc@gmail.com>
2019-02-28 22:39:26 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,19 @@
 module Findable
 
-    def self.all
+  @@artists = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def self.all
     @@artists
   end
-  
+ 
   def self.find_by_name(name)
     @@artists.detect {|a| a.name == name}
   end
-  
-
   
 end
