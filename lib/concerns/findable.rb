01570ea7b4d9314b5be6abc85e8006d698e5dbe2
Sara Harvey <saraharveynyc@gmail.com>
2019-03-01 00:49:09 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,13 +4,4 @@ module Findable
     self.all.detect {|a| a.name == name}
   end
   
-  #module Findable
-  #module ClassMethods
-  #  def find_by_name(name)
-  #    self.all.detect{|o| o.name}
-  #  end    
-  #end
-#end
-  
-  
 end
